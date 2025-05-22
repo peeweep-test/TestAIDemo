484f665c1b84c5997b23c592dbf50b73bd33272c
Tian Shilin <tianshilin@uniontech.com>
2025-05-23 00:03:27 +0800

    fix

--- translations/TestAIDemo_az.ts
+++ translations/TestAIDemo_az.ts
@@ -8,5 +8,10 @@
         <source>Hello, welcome to the application!</source>
         <translation>Salam, tətbiqə xoş gəlmisiniz!</translation>
     </message>
+    <message>
+        <location filename="../main.cpp" line="13"/>
+        <source>This is a warning message.</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 </TS>
--- translations/TestAIDemo_zh_TW.ts
+++ translations/TestAIDemo_zh_TW.ts
@@ -8,5 +8,10 @@
         <source>Hello, welcome to the application!</source>
         <translation>你好，歡迎使用本應用程式！</translation>
     </message>
+    <message>
+        <location filename="../main.cpp" line="13"/>
+        <source>This is a warning message.</source>
+        <translation type="unfinished"></translation>
+    </message>
 </context>
 </TS>
