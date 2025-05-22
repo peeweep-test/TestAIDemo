7311cc626fb97afb97a96495b4a3cff0957af771
Tian Shilin <tianshilin@uniontech.com>
2025-05-23 00:48:28 +0800

    2

--- translations/TestAIDemo_en.ts
+++ translations/TestAIDemo_en.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="zh_CN">
+<TS version="2.1" language="en_US">
 <context>
     <name>QObject</name>
     <message>
         <location filename="../main.cpp" line="12"/>
         <source>Hello, welcome to the application!</source>
-        <translation>你好，欢迎使用本应用！</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../main.cpp" line="13"/>
