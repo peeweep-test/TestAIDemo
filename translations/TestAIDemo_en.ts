d35a0b3cb835525387d3e02adff64ee2eef6fe65
Tian Shilin <tianshilin@uniontech.com>
2025-05-23 00:00:37 +0800

    fix

--- translations/TestAIDemo_en.ts
+++ translations/TestAIDemo_en.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../main.cpp" line="12"/>
         <source>Hello, welcome to the application!</source>
-        <translation type="unfinished"></translation>
+        <translation>Hello, welcome to the application!</translation>
     </message>
 </context>
 </TS>
